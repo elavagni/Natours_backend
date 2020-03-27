f308bbae7d58e9d18d3168d0bc2c2c2d4002742f
eric <elavagnim@gmail.com>
2020-03-26 23:19:21 -0500

    Added mongoose document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration']
@@ -63,6 +65,23 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
+
+//DOCUMENT MIDDLEWARE: runs before the .save() and .create() but not on .insertMany()
+tourSchema.pre('save', function(next) {
+  //this represents the document that is being process
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+//Leave this code as reference
+// tourSchema.pre('save', function(next) {
+//   console.log('Will save document soon...');
+//   next();
+// });
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
