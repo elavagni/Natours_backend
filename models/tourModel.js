3a1f0809b57dc82bfe8f7dc4d7f1f91c749ce259
eric <elavagnim@gmail.com>
2020-03-28 21:19:29 -0500

    Add custom validators.  Install validator

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
     name: {
       type: String,
       required: [true, 'A tour must have a name'],
-      unique: true
+      unique: true,
+      maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+      minlength: [10, 'A tour name must have less or equal than 10 characters']
     },
     slug: String,
     duration: {
@@ -19,11 +22,17 @@ const tourSchema = new mongoose.Schema(
     },
     difficulty: {
       type: String,
-      required: [true, 'A tour must have a difficulty']
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult'
+      }
     },
     ratingsAverage: {
       type: Number,
-      default: 4.5
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below or eqaul to 5.0']
     },
     ratingsQuantity: {
       type: Number,
@@ -33,7 +42,18 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price']
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function(discount) {
+          // the keyword 'this' only points to current doc on NEW document creation, the validator will not work
+          // when updating documents
+          return discount < this.price;
+        },
+        message:
+          'Discount price ({VALUE}) should be less than the regular price'
+      }
+    },
     summary: {
       type: String,
       trim: true,
