2d7f302c983e443098f30aba0e4c1524b293f84c
eric <elavagnim@gmail.com>
2020-12-05 13:13:31 -0600

    Add middleware to restrict routes based on user role

--- models/userModel.js
+++ models/userModel.js
@@ -17,6 +17,11 @@ const userSchema = new mongoose.Schema({
   photo: {
     type: String
   },
+  role: {
+    type: String,
+    enum: ['user', 'guide', 'leader-guide', 'admin'],
+    default: 'user'
+  },
   password: {
     type: String,
     required: [true, 'Please provide a password'],
