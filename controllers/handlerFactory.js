bf315b068f5b9b579a84c1a5175b0561e45d9397
eric <elavagnim@gmail.com>
2020-12-19 21:40:12 -0600

    Add factory functions for reading documents (getOne, getAll)

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,5 +1,6 @@
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.deleteOne = Model =>
   catchAsync(async (req, res, next) => {
@@ -44,6 +45,54 @@ exports.createOne = Model =>
       status: 'success',
       data: {
         data: doc
+      }
+    });
+  });
+
+exports.getOne = (Model, populateOptions) =>
+  catchAsync(async (req, res, next) => {
+    //First create query without populate options
+    let query = Model.findById(req.params.id);
+
+    //If there are populate options (reference docs) add them to the query
+    if (populateOptions) query = query.populate(populateOptions);
+
+    //execute query
+    const doc = await query;
+
+    if (!doc) {
+      return next(new AppError('No document found with that id', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc
+      }
+    });
+  });
+
+exports.getAll = Model =>
+  catchAsync(async (req, res, next) => {
+    //To allow for nested GET reviews on tour (hack - only necessary for the reviews route)
+    let filter = {};
+    //If there is a tour id in the query string, only retrieve the reviews that belong to that tour
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+
+    // EXECUTE QUERY
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const documents = await features.query;
+
+    //SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      results: documents.length,
+      data: {
+        data: documents
       }
     });
   });
