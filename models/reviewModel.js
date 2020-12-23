4a24acb482ce70a344ba3bd051835d33d990b6e3
eric <elavagnim@gmail.com>
2020-12-22 21:49:54 -0600

    Add unique index to prevent duplicate reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -32,6 +32,9 @@ const reviewSchema = new mongoose.Schema(
     toObject: { virtuals: true }
   }
 );
+
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function(next) {
   this.populate({
     path: 'user',
