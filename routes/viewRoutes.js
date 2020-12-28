2aa96abc5d6221079666c9933e34318ff4de09cf
eric <elavagnim@gmail.com>
2020-12-27 22:55:00 -0600

    Update routes for all tours and tour details

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,14 +3,7 @@ const viewsController = require('../controllers/viewsController');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker tour',
-    user: 'John'
-  });
-});
-
-router.get('/overview', viewsController.getOverview);
-router.get('/tour', viewsController.getTour);
+router.get('/', viewsController.getOverview);
+router.get('/tour/:slug', viewsController.getTour);
 
 module.exports = router;
