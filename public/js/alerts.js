29b0b8a33ea65059514b5549bd1aca7fbb1d5b06
eric <elavagnim@gmail.com>
2021-01-02 17:50:42 -0600

    export modules to be included in index.js files

---
+++ public/js/alerts.js
@@ -0,0 +1,12 @@
+export const hideAlert = () => {
+  const element = document.querySelector('.alert');
+  if (element) element.parentElement.removeChild(element);
+};
+
+//type is 'success' or 'error'
+export const showAlert = (type, message) => {
+  hideAlert();
+  const markup = `<div class="alert alert--${type}">${message}</div>`;
+  document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+  window.setTimeout(hideAlert, 5000);
+};
