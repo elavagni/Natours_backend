95d6b01a2a24591c12b54d9893e48edf75a9645c
eric <elavagnim@gmail.com>
2021-01-03 15:25:07 -0600

    Front-end changes to log user out

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,11 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
-import { login } from './login';
+import { login, logout } from './login';
 
 // DOM elements
 const mapbox = document.getElementById('map');
 const loginForm = document.querySelector('.form');
+const logoutBtn = document.querySelector('.nav__el--logout');
 
 // delegation
 if (mapbox) {
@@ -22,3 +23,5 @@ if (loginForm) {
     login(email, password);
   });
 }
+
+if (logoutBtn) logoutBtn.addEventListener('click', logout);
