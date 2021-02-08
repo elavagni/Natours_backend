7393e5631e18b5753da9e12d3647100b422c2054
eric <elavagnim@gmail.com>
2021-02-07 23:08:43 -0600

    Upload user photo, and update user model

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,59 @@
+const multer = require('multer');
+const sharp = require('sharp');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
+
+/* upload image without image processing 
+
+const multerStorage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, 'public/img/users');
+  },
+  filename: (req, file, callback) => {
+    //user-userId-datetime.jpeg
+    const extension = file.mimetype.split('/')[1];
+    callback(null, `user-${req.user.id}-${Date.now()}.${extension}`);
+  }
+});
+*/
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, callback) => {
+  if (file.mimetype.startsWith('image')) {
+    callback(null, true);
+  } else {
+    callback(
+      new AppError('Not an image! Please upload only images.', 400),
+      false
+    );
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter
+});
+
+exports.resizeUserPhoto = (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({
+      quality: 90
+    })
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+};
+
+exports.uploadUserPhoto = upload.single('photo');
 
 //Create object with only the fields that are allowed to be updated
 const filterObj = (obj, ...allowedFields) => {
@@ -34,6 +86,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   //Filter out unwanted field names that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
 
   // Since we are not updating the password here, we can use User.findByIdAndUpdate
   // as the pre save middleware is not necessary, so it is ok if it doesn't run
