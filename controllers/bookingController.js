3a0a06d97dcc279ee2bc0ebed25e6da46cc58b2f
eric <elavagnim@gmail.com>
2021-02-22 20:18:44 -0600

    Implement CRUD routes for booking controller

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -50,3 +50,19 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 
   res.redirect(req.originalUrl.split('?')[0]);
 });
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  // this is temporary, everybody could make bookings without paying
+  const { tour, user, price } = req.query;
+
+  if (!tour && !user && !price) return next();
+  await Booking.create({ tour, user, price });
+
+  res.redirect(req.originalUrl.split('?')[0]);
+});
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
